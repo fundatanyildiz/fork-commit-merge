1ff299b79bb473e58b356ca09d763e15b26f634f
Funda variş <funda.varis@gmail.com>
2023-09-22 12:01:12 +0200

    Fixed issue

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -1,3 +1,12 @@
 # Python - Easy
 
 # TODO: Create the Simple Python Program to Interact with Lists
+def interaction_of_lists():
+    list_of_numbs = []
+    sum = 0
+    for i in range(1, 11):
+        list_of_numbs.append(i)
+        sum += i
+    print('Created list from natural numbers: ', list_of_numbs)
+    print('Sum of the numbers in the list: ', sum)
+
